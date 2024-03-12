8941607814f6bf78f72e032708460c8ec3cb165a
Jorn de Bruin <131358972+JornDeBruin1@users.noreply.github.com>
2024-03-12 13:46:58 +0100

    Update config.php

--- etc/config.php
+++ etc/config.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost:3306');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'net24jbruin_Jorn');
 define('DB_PASSWORD', '6O=xp0Le&2*4');
-define('DB_NAME', 'php_oop_example');
+define('DB_NAME', 'net24jbruin_php_oop_example');
  
 /* Attempt to connect to MySQL database */
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
